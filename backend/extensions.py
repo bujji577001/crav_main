8bd4c9abc86d93f609572b2ccb5b2556356fab99
bujji577001 <somaprathibha577001@gmail.com>
2025-11-09 22:59:11 +0530

    Update extensions.py

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,14 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security
 from flask_restful import Api
-# --- ✅ START: IMPORT FLASK-MIGRATE ---
+from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
-# --- ✅ END: IMPORT FLASK-MIGRATE ---
 
 # Create the extension instances here
 db = SQLAlchemy()
 security = Security()
 api = Api()
-# --- ✅ START: INSTANTIATE MIGRATE ---
+jwt = JWTManager()
 migrate = Migrate()
-# --- ✅ END: INSTANTIATE MIGRATE ---
